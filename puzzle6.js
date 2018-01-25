510c6a67de5194265dd8446dd475dc0865805821
CJordanGoodman <good.cjordan@gmail.com>
2018-01-25 10:31:33 -0800

    Generalize puzzle6.js to implied test case

--- puzzle6.js
+++ puzzle6.js
@@ -7,16 +7,19 @@ function performActionNTimes(action, n) {
 }
 
 right();
-if(getColor == "red") {
+var firstColor = getColor();
+if(firstColor == "red") {
   performActionNTimes(up, 3);
-  if(getColor == "red") {
-    left();
-  } else {
-    right();
-  }
-  up();
 } else {
   performActionNTimes(down, 3);
+}
+if (getColor() == "red") {
   left();
+} else{
+  right();
+}
+if (firstColor == "red") {
+  up();
+} else{
   down();
 }
